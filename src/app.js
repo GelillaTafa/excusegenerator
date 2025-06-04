89c7fc1a4a5cf737b416a1e38bb4f7b84e895b4c
GelillaTafa <gelillatafa@gmail.com>
2025-06-04 22:05:11 +0000

    add  excuses

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+ 
+  document.querySelector('#excuse').innerHTML=generateExcuse();
+  console.log(generateExcuse())
+
 };
+
+const generateExcuse=()=>{
+  const who=["Mickey Mouse", "Donals Duck", "Goofy", "Moana","Elsa"];
+  const what=["my project", "my home work", "my laptop", "my car key", "my flash drive"];
+  const when=["last night", "this morning","during lunch break","before the exam", "before class"];
+  const action =["lost","ate ", "broke ","froze","burned"]
+
+
+  let random= Math.floor(Math.random()*5)
+  let excuse;
+  
+      excuse=who[random] +' '+  action[random] +' '+ what[random] + ' ' + when[random]+'!!!!'
+      
+  return excuse;
+}
